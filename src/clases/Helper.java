b06af697fdde156e056a779adeef069af71a85b2
“Jaime” <jtorres61@cuc.edu.co>
2016-09-27 18:32:28 -0500

    Clase creada y metodos basicos para la funcionalidad del taller creados

---
+++ src/clases/Helper.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.awt.Component;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author jaime
+ */
+public class Helper {
+
+    public static void mensaje(Component ventana, String mensaje, int tipo) {
+        switch (tipo) {
+            case 1:
+                JOptionPane.showMessageDialog(ventana, mensaje, "Información", JOptionPane.INFORMATION_MESSAGE);
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(ventana, mensaje, "Advertencia", JOptionPane.WARNING_MESSAGE);
+                break;
+            case 3:
+                JOptionPane.showMessageDialog(ventana, mensaje, "Error", JOptionPane.ERROR_MESSAGE);
+                break;
+
+        }
+    }
+
+    public static void limpiadoTabla(JTable tabla1) {
+        int nf = tabla1.getRowCount();
+        int nc = tabla1.getColumnCount();
+
+        for (int i = 0; i < nf; i++) {
+            for (int j = 0; j < nc; j++) {
+                tabla1.setValueAt("", i, j);
+            }
+
+        }
+    }
+
+    public static void porDefectoTabla(JTable tabla1) {
+        DefaultTableModel tm;
+        tm = (DefaultTableModel) tabla1.getModel();
+        tm.setRowCount(0);
+        tm.setColumnCount(0);
+    }
+
+    public static void habilitarBotones(JButton[] botones) {
+        for (int i = 0; i < botones.length; i++) {
+            botones[i].setEnabled(true);
+        }
+    }
+
+    public static void deshabilitarBotones(JButton[] botones) {
+        for (int i = 0; i < botones.length; i++) {
+            botones[i].setEnabled(false);
+        }
+    }
+
+    public static int cantidadPares(JTable tabla1) {
+        int aux, cont = 0;
+        int nf = tabla1.getRowCount();
+        int nc = tabla1.getColumnCount();
+
+        for (int i = 0; i < nf; i++) {
+            for (int j = 0; j < nc; j++) {
+                aux = (int) tabla1.getValueAt(i, j);
+                if (aux % 2 == 0) {
+                    cont++;
+                }
+            }
+        }
+        return cont;
+    }
+
+    public static void pares(JTable tabla1) {
+        int aux;
+        int nf = tabla1.getRowCount();
+        int nc = tabla1.getColumnCount();
+
+        for (int i = 0; i < nf; i++) {
+            for (int j = 0; j < nc; j++) {
+                aux = (int) tabla1.getValueAt(i, j);
+                if (aux % 2 == 0) {
+                    tabla1.setValueAt(aux, i, j);
+                }
+            }
+        }
+    }
+}
